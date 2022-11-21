3a3b731255f46fe50f694528cef4a0aa68c0dab2
Knox Hutchinson <knox@knoxsdata.com>
2022-11-21 14:34:49 -0600

    ship and deploy part 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        <h1>Hello Knox</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
